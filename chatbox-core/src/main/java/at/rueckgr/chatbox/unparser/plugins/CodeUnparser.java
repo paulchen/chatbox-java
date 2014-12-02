28d6c8eab12b7d6e5b5d9c3dcc97b9683e4553e1
Paul Staroch <paulchen@rueckgr.at>
2014-12-02 18:05:20 +0100

    introduced CodeUnparser

---
+++ chatbox-core/src/main/java/at/rueckgr/chatbox/unparser/plugins/CodeUnparser.java
@@ -0,0 +1,33 @@
+package at.rueckgr.chatbox.unparser.plugins;
+
+import org.apache.commons.lang3.StringEscapeUtils;
+
+import javax.enterprise.context.ApplicationScoped;
+import java.text.MessageFormat;
+import java.util.regex.Matcher;
+
+@Unparser
+@ApplicationScoped
+public class CodeUnparser extends AbstractSearchReplaceUnparser {
+    private static final String PATTERN = "<!-- BEGIN TEMPLATE: bbcode_code -->\\s*" +
+                    "<div class=\"bbcode_container\">\\s*" +
+                    "<div class=\"bbcode_description\">Code:</div>\\s*" +
+                    "<pre class=\"bbcode_code\" style=\"height:84px;\">" +
+                    "([^<]*)" +
+                    "</pre>\\s*" +
+                    "</div>\\s*" +
+                    "<!-- END TEMPLATE: bbcode_code -->";
+
+    private static final String REPLACEMENT = "[code]{0}[/code]";
+
+    @Override
+    protected String getPattern() {
+        return PATTERN;
+    }
+
+    @Override
+    protected String getReplacement(Matcher matcher) {
+        String match = matcher.group(1);
+        return MessageFormat.format(REPLACEMENT, StringEscapeUtils.unescapeHtml4(match));
+    }
+}
---
+++ chatbox-core/src/test/java/at/rueckgr/chatbox/unparser/plugins/CodeUnparserTest.java
@@ -0,0 +1,51 @@
+package at.rueckgr.chatbox.unparser.plugins;
+
+import at.rueckgr.chatbox.test.ContainerTest;
+import org.testng.annotations.Test;
+
+import javax.inject.Inject;
+
+import static org.testng.Assert.*;
+
+public class CodeUnparserTest extends ContainerTest {
+    private @Inject CodeUnparser codeUnparser;
+
+    @Test
+    public void testUnparseEmpty() {
+        assertEquals(codeUnparser.unparse(""), "");
+    }
+
+    @Test
+    public void testUnparseNoTag() {
+        String test = "this is some text";
+
+        assertEquals(codeUnparser.unparse(test), test);
+    }
+
+    @Test
+    public void testUnparse() {
+        String test = "this is some text before the tag " +
+                "<!-- BEGIN TEMPLATE: bbcode_code -->\n" +
+                "<div class=\"bbcode_container\">\n" +
+                "\t<div class=\"bbcode_description\">Code:</div>\n" +
+                "\t<pre class=\"bbcode_code\" style=\"height:84px;\">&lt;script type=&quot;text/javascript&quot;&gt;\n" +
+                "&lt;!--\n" +
+                "\talert(&quot;Hello world!&quot;);\n" +
+                "//--&gt;\n" +
+                "&lt;/script&gt;</pre>\n" +
+                "</div>\n" +
+                "<!-- END TEMPLATE: bbcode_code -->" +
+                "and this is some text after the tag";
+
+        String expected = "this is some text before the tag " +
+                "[code]<script type=\"text/javascript\">\n" +
+                "<!--\n" +
+                "\talert(\"Hello world!\");\n" +
+                "//-->\n" +
+                "</script>[/code]" +
+                "and this is some text after the tag";
+
+        assertEquals(codeUnparser.unparse(test), expected);
+    }
+
+}
