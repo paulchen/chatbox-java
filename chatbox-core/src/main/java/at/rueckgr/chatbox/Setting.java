81208db7b01924c1b3b34994c2bc07f79601fcfe
Paul Staroch <paulchen@rueckgr.at>
2014-12-07 15:18:33 +0100

    query stats pages after posting daily stats to chatbox

--- chatbox-core/src/main/java/at/rueckgr/chatbox/Setting.java
+++ chatbox-core/src/main/java/at/rueckgr/chatbox/Setting.java
@@ -4,6 +4,7 @@ import lombok.Getter;
 
 @Getter
 public enum Setting {
+    // TODO add type, default value
     FORUM_USERNAME("forum_username", true),
     FORUM_PASSWORD("forum_password", true),
     LAST_UPDATE("last_update", false),
@@ -11,7 +12,12 @@ public enum Setting {
     ENVIRONMENT("environment", true),
     BOT_ACTIVE("bot_active", true),
     BASE_URL("base_url", true),
-    DAILY_STATS_RANKS("daily_stats_ranks", true);
+    DAILY_STATS_RANKS("daily_stats_ranks", true),
+    HOSTNAME("hostname", true),
+    PORT("port", true),
+    SCHEME("scheme", true),
+    UPDATE_USERNAME("update_username", true),
+    UPDATE_PASSWORD("update_password", true);
 
     private final String databaseName;
     private final boolean required;
