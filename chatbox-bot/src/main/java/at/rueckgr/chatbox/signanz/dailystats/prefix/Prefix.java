f651ef6bc4b57a5e8f42b2adba7484154e8124d3
Paul Staroch <paulchen@rueckgr.at>
2014-12-07 15:37:18 +0100

    fixed last commit

---
+++ chatbox-bot/src/main/java/at/rueckgr/chatbox/signanz/dailystats/prefix/Prefix.java
@@ -0,0 +1,7 @@
+package at.rueckgr.chatbox.signanz.dailystats.prefix;
+
+/**
+ * Created by paulchen on 06.12.14.
+ */
+public class Prefix {
+}
--- chatbox-core/src/test/sql/01_settings.sql
+++ chatbox-core/src/test/sql/01_settings.sql
@@ -1,4 +1,4 @@
-MERGE INTO SETTINGS (key, value) VALUES ('environment', 'dev');
+MERGE INTO SETTINGS (key, value) VALUES ('environment', 'unit-test');
 MERGE INTO SETTINGS (key, value) VALUES ('current_epoch', '1');
 MERGE INTO SETTINGS (key, value) VALUES ('forum_password', 'TODO');
 MERGE INTO SETTINGS (key, value) VALUES ('forum_username', 'TODO');
