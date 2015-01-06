99119ee853f13d9aff9a23f26680a9737f61f5b6
Paul Staroch <paulchen@rueckgr.at>
2015-01-06 23:24:28 +0100

    log exceptions in ExecutionSafeExecutorService

--- core/src/main/java/at/rueckgr/chatbox/util/ExceptionSafeExecutorService.java
+++ core/src/main/java/at/rueckgr/chatbox/util/ExceptionSafeExecutorService.java
@@ -1,6 +1,7 @@
 package at.rueckgr.chatbox.util;
 
 import at.rueckgr.chatbox.service.MailService;
+import org.apache.commons.logging.Log;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.inject.Inject;
@@ -8,6 +9,7 @@ import java.util.concurrent.Callable;
 
 @ApplicationScoped
 public class ExceptionSafeExecutorService {
+    private @Inject Log log;
     private @Inject MailService mailService;
 
     public <T> T execute(Callable<T> callable, T defaultOutput) {
@@ -15,6 +17,8 @@ public class ExceptionSafeExecutorService {
             return callable.call();
         }
         catch (Throwable e) {
+            log.error("Exception occurred", e);
+
             mailService.sendExceptionMail(e);
 
             return defaultOutput;
